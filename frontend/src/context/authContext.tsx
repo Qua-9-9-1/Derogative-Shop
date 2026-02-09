7ca4d0a836272bf482394f0cda6c8cfe66ce1762
Quentin D <quentin1.duval@epitech.eu>
2026-02-09 15:12:32 +0100

    [FIX] usage of local storage for webapp

--- frontend/src/context/authContext.tsx
+++ frontend/src/context/authContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import * as SecureStore from 'expo-secure-store';
 import { authService } from '../services/authService';
+import { Platform } from 'react-native';
 
 interface AuthContextData {
   token: string | null;
@@ -12,14 +13,38 @@ interface AuthContextData {
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
+async function saveItem(key: string, value: string) {
+  if (Platform.OS === 'web') {
+    localStorage.setItem(key, value);
+  } else {
+    await SecureStore.setItemAsync(key, value);
+  }
+}
+
+async function getItem(key: string) {
+  if (Platform.OS === 'web') {
+    return localStorage.getItem(key);
+  } else {
+    return await SecureStore.getItemAsync(key);
+  }
+}
+
+async function deleteItem(key: string) {
+  if (Platform.OS === 'web') {
+    localStorage.removeItem(key);
+  } else {
+    await SecureStore.deleteItemAsync(key);
+  }
+}
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(null);
   const [userId, setUserId] = useState<string | null>(null);
 
   useEffect(() => {
     const loadAuthData = async () => {
-      const storedToken = await SecureStore.getItemAsync('user_token');
-      const storedUserId = await SecureStore.getItemAsync('user_id');
+      const storedToken = await getItem('user_token');
+      const storedUserId = await getItem('user_id');
 
       if (storedToken && storedUserId) {
         setToken(storedToken);
@@ -36,18 +61,18 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       setToken(result.token);
       setUserId(result.user.id);
 
-      await SecureStore.setItemAsync('user_token', result.token);
-      await SecureStore.setItemAsync('user_id', result.user.id);
+      await saveItem('user_token', result.token);
+      await saveItem('user_id', result.user.id);
     } else {
-      throw new Error("Échec de la connexion");
+      throw new Error('Login failed');
     }
   };
 
   const logout = async () => {
     setToken(null);
     setUserId(null);
-    await SecureStore.deleteItemAsync('user_token');
-    await SecureStore.deleteItemAsync('user_id');
+    await deleteItem('user_token');
+    await deleteItem('user_id');
   };
 
   return (
