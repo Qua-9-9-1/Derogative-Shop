5a20dc4ec5b0087c5c74f6f7454a76a556a8031f
Quentin D <quentin1.duval@epitech.eu>
2026-02-25 15:11:24 +0100

    [EDIT] find product from barcode in db only

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -1,8 +1,5 @@
 import { prisma } from '../prismaClient';
 import { Product } from '@prisma/client';
-import axios from 'axios';
-
-const OFF_API_URL = 'https://world.openfoodfacts.org/api/v0/product';
 
 export const productService = {
   async getOrFetchProduct(barcode: string): Promise<Product | null> {
@@ -15,35 +12,7 @@ export const productService = {
       return existingProduct;
     }
 
-    console.log(`product ${barcode} not found locally. Calling OFF...`);
-    try {
-      const response = await axios.get(`${OFF_API_URL}/${barcode}.json`);
-
-      if (response.data.status === 1) {
-        const offData = response.data.product;
-
-        const newProduct = await prisma.product.create({
-          data: {
-            id: barcode,
-            name: offData.product_name || 'Unknown product',
-            brand: offData.brands || 'Unknown brand',
-            smallImageUrl: offData.image_front_small_url || null,
-            imageUrl: offData.image_front_url || null,
-            price: 999,
-            category: offData.categories_tags ? offData.categories_tags[0] : null,
-            nutritionalInfo: offData.nutriments || {},
-            stockQuantity: 0,
-          },
-        });
-
-        return newProduct;
-      }
-
-      return null;
-    } catch (error) {
-      console.error('Erreur API OFF', error);
-      return null;
-    }
+    return null;
   },
 
   async getAllProducts() {
